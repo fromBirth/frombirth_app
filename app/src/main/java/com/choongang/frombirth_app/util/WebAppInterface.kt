0db2d6ead3a8f513ec8f0f866ad1f3b322a222bf
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-11-06 14:47:01 +0900

    1106 baek 업로드 관련 오류 수정 Fix

--- app/src/main/java/com/choongang/frombirth_app/util/WebAppInterface.kt
+++ app/src/main/java/com/choongang/frombirth_app/util/WebAppInterface.kt
@@ -7,7 +7,7 @@ import com.choongang.frombirth_app.MainActivity
 class WebAppInterface(private val activity: Activity) {
 
     @JavascriptInterface
-    fun openFileChooser() { //안드로이드 파일 탐색기 사용
-        (activity as MainActivity).openFileChooser()
+    fun openFileChooser(isVideo: Boolean) { //안드로이드 파일 탐색기 사용
+        (activity as MainActivity).openFileChooser(isVideo)
     }
 }
