fadb76c3fcfc5645e7a7e49776202b99565da9c3
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-11-06 10:48:57 +0900

    1106 baek 안드로이드 파일 탐색기 사용 Update

---
+++ app/src/main/java/com/choongang/frombirth_app/util/WebAppInterface.kt
@@ -0,0 +1,13 @@
+package com.choongang.frombirth_app.util
+
+import android.app.Activity
+import android.webkit.JavascriptInterface
+import com.choongang.frombirth_app.MainActivity
+
+class WebAppInterface(private val activity: Activity) {
+
+    @JavascriptInterface
+    fun openFileChooser() { //안드로이드 파일 탐색기 사용
+        (activity as MainActivity).openFileChooser()
+    }
+}
